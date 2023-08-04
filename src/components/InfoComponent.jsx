d2b094c05975d21877a1aecadcf52005d02368ab
CodableBase <asc.rddf@gmail.com>
2023-08-04 15:07:11 +0300

    Ended of creating Web-Site 2

--- src/components/InfoComponent.jsx
+++ src/components/InfoComponent.jsx
@@ -5,7 +5,7 @@ const InfoComponent = ({onClick, state}) => {
     return (
         <div className="InfoMenu">
             <h4>Download App from Google Play -> <a href="www.google.com">Here</a></h4>
-            <h4>Download App from App Store -> <a href="www.google.com">Will be added later</a></h4>
+            <h4>Download App from App Store -> <a href="">Will be added later</a></h4>
             <h4>Download from the website -> <a href="www.google.com">Download</a></h4>
             <Button text="Close" onClick={onClick} state={state}></Button>
         </div>
